0b3369b7d8a7819b4c02b428ebbdb789647894de
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2023-11-21 12:00:39 -0300

    Conclus~so
    Conclusão do primeiro modulo

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+	"strconv"
 )
 
-func main() {
+const monitoramentos = 3
+const delay = 5
 
-	exibeIntroducao()
+func main() {
+	exibeIntroducao() 
 	for {
 		exibeMenu()
 		comando := leComando()
@@ -17,7 +24,7 @@ func main() {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -25,9 +32,7 @@ func main() {
 			fmt.Println("Não conheço este comando")
 			os.Exit(-1)
 		}
-
 	}
-
 }
 
 func exibeIntroducao() {
@@ -52,20 +57,82 @@ func leComando() int {
 }
 
 func iniciarMonitoramento() {
-	site := []string{"https://qualitec.cloudns.cl/login", "https://www.alura.com.br"}
-	
-	for i, sites := range site {
-		fmt.Println("Testando Site:", i, ":",sites)
-		testeSite(sites)
+
+	site := leSiteDoArquivo()
+
+	for i := 0; i < monitoramentos; i++ {
+
+		for i, sites := range site {
+			fmt.Println("Testando Site:", i, ":", sites)
+			testeSite(sites)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+
 	}
+	fmt.Println("")
 
 }
 
 func testeSite(site string) {
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "Foi carregado com sucesso!")
+		registraLogs(site , true)
 	} else {
 		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+		registraLogs(site , false)
 	}
+}
+
+func leSiteDoArquivo() []string {
+	var sites []string
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		fmt.Println(linha)
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	return sites
+}
+
+func registraLogs(site string, status bool){
+
+	arquivo ,err := os.OpenFile("log.txt",os.O_RDWR|os.O_CREATE| os.O_APPEND, 0666)
+	
+	if err != nil {
+		fmt.Println("Ocorreu um nos logs, erro:", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - "+ site + "- Online: "+ strconv.FormatBool(status) +"\n" )
+	arquivo.Close()
+}
+
+
+func imprimeLogs()  {
+	arquivo, err := os.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo) )
+
 }
